dec656e7f93089aa51c533249047d19dfc05aa4d
Hossein Mazhar <hosseinmazhar1053@gmail.com>
2025-02-23 17:03:45 +0330

    landing page remove zombie

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SquareArrowOutUpRight } from "lucide-react";
-import NewArrivalsSlider from "./NewArrivalsSlider";
 
 export default function LandingPage() {
   return (
